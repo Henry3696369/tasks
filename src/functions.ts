d9b202a5bb7b7351c46fb6c3a2b640224a9436a6
Henry <luoheng36963@gmail.com>
2026-02-11 22:35:08 -0500

    task4 finished

--- src/functions.ts
+++ src/functions.ts
@@ -2,9 +2,12 @@
  * Consumes a single temperature in Fahrenheit (a number) and converts to Celsius
  * using this formula:
  *      C = (F - 32) * 5/9
+ *
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    let result: number = ((temperature - 32) * 5) / 9;
+
+    return result;
 }
 
 /**
@@ -12,7 +15,10 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    first = first > 0 ? first : 0;
+    second = second > 0 ? second : 0;
+    third = third > 0 ? third : 0;
+    return first + second + third;
 }
 
 /**
@@ -20,7 +26,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +34,10 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    if (message[message.length - 1] === "?") {
+        return true;
+    }
+    return false;
 }
 
 /**
@@ -37,5 +46,10 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    if (word.toLowerCase() === "yes") {
+        return true;
+    } else if (word.toLocaleLowerCase() === "no") {
+        return false;
+    }
+    return null;
 }
